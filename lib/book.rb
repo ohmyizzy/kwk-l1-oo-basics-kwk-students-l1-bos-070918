bc59cbef12bd84b638b43e2f9153eb2ce5b302e3
ohmyizzy <conneri19@barringtonschools.org>
2018-07-13 13:56:01 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
 # book.rb
 # Add you Book class here
+
+
+class Book
+  
+  attr_reader :title
+  
+  attr_accessor :language, :author, :genre, :page_count
+  
+  def initialize(title, author, genre, page_count)
+    @title = title
+    @author = author
+    @genre = genre
+    @page_count = page_count
+  end 
+  
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+  
+end 
+
+great_gatsby = Book.new("The Great Gatsby", "Fitzgerald", "Fiction", 200)
